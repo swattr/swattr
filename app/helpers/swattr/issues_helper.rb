99f6c17db8ff776bacf84c10368440c91d2e572a
David Freerksen <dfreerksen@gmail.com>
2015-11-03 22:07:08 -0800

    Issue helper specs

--- app/helpers/swattr/issues_helper.rb
+++ app/helpers/swattr/issues_helper.rb
@@ -2,7 +2,7 @@ module Swattr
   module IssuesHelper
     include HumanBytes
 
-    def filesize_format(file_size = 0, places = 1, i = false)
+    def filesize_format(file_size = 0, places = 0, i = false)
       HumanBytes.human_bytes(file_size.to_i, places: places, i: i)
     end
   end
--- spec/helpers/swattr/issues_helper_spec.rb
+++ spec/helpers/swattr/issues_helper_spec.rb
@@ -2,5 +2,30 @@ require "rails_helper"
 
 module Swattr
   RSpec.describe IssuesHelper, type: :helper do
+    describe "#filesize_format" do
+      it "with zero byte size file" do
+        expect(helper.filesize_format(0)).to eq "0 B"
+      end
+
+      it "with 1000 byte size file" do
+        expect(helper.filesize_format(1000)).to eq "1 KB"
+      end
+
+      it "with 1000000 byte size file" do
+        expect(helper.filesize_format(1000000)).to eq "1 MB"
+      end
+
+      it "with 1000000000 byte size file" do
+        expect(helper.filesize_format(1000000000)).to eq "1 GB"
+      end
+
+      it "with 1234 byte size file" do
+        expect(helper.filesize_format(1234, 1)).to eq "1.2 KB"
+      end
+
+      it "with byte 1234 size file" do
+        expect(helper.filesize_format(1234, 1, true)).to eq "1.2 KiB"
+      end
+    end
   end
 end
