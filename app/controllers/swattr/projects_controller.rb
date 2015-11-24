48daba2d308d01f736813f39e0995dcddca5de8b
David Freerksen <dfreerksen@gmail.com>
2015-11-23 20:51:47 -0800

    Projects not using Resourceful

--- app/controllers/swattr/projects_controller.rb
+++ app/controllers/swattr/projects_controller.rb
@@ -1,12 +1,7 @@
 module Swattr
   class ProjectsController < ApplicationController
-    include Swattr::Concern::Resourceful
-
-    def find_by
-      :slug
-    end
-
     before_action :add_project_defaults_on_create, only: [:create]
+    before_action :set_project, only: [:show, :new, :edit, :update, :destroy]
 
     def index
       @q = Swattr::Project.ransack(params[:q].try(:merge, m: "or"))
@@ -18,12 +13,60 @@ module Swattr
       respond_with @projects
     end
 
+    def show
+      respond_with @project
+    end
+
+    def new
+      respond_with @project
+    end
+
+    def edit
+      respond_with @project
+    end
+
+    def create
+      @project = Swattr::Project.new(project_params)
+
+      authorize @project
+
+      @project.save
+
+      respond_with @project, location: projects_path
+    end
+
+    def update
+      @project.update(project_params)
+
+      respond_with @project, location: projects_path
+    end
+
+    def destroy
+      @project.destroy
+
+      respond_with @project, location: projects_path
+    end
+
     protected
 
     def permitted_attributes
       [
         :name, :slug, :description, :location, :hero, :remove_hero, :author_id
       ]
+    end
+
+    def set_project
+      if action_name.to_sym == :new
+        @project = Swattr::Project.new
+      else
+        @project = Swattr::Project.find_by(slug: params[:id])
+      end
+
+      authorize @project
+    end
+
+    def project_params
+      params.require(:project).permit(permitted_attributes)
     end
 
     def add_project_defaults_on_create
