588dc2edc3e8ea6ad6d611260963151cd1574f26
David Freerksen <dfreerksen@gmail.com>
2015-11-27 17:24:21 -0800

    Correct label

--- spec/lib/tasks/swattr_tasks_spec.rb
+++ spec/lib/tasks/swattr_tasks_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Swattr Rake task", type: :rake do
   include_context "rake"
 
   describe "swattr:version" do
-    it "creates provider" do
+    it "find current version" do
       expect { subject.execute }.to match_stdout Swattr::VERSION
     end
   end
