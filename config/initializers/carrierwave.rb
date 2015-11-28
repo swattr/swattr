ef8820f2710b08dcdeda17c6ef448fa78d429fee
David Freerksen <dfreerksen@gmail.com>
2015-11-27 18:29:55 -0800

    Remove S3_REGION

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,10 @@
 CarrierWave.configure do |config|
+  # Local file storage
   config.permissions = 0666
   config.directory_permissions = 0777
   config.storage = :file
 
+  # Use local file storage for tests
   if Rails.env.test?
     config.storage = :file
     config.enable_processing = false
